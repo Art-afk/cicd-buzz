9eaf24b5eee83362a4b6a2f82fb3431289f41e11
Art-afk <94371440+Art-afk@users.noreply.github.com>
2021-11-21 18:46:38 +0800

    add Docker

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-import signal
+#import signal
 from flask import Flask
 from buzz import generator
 
 app = Flask(__name__)
 
-signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+#signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
-@app.route("/")
+@app.route("/") 
 def generate_buz():
-    page = '<html><body><h1>'
+    page = '<html>'
     page += generator.generate_buzz()
-    page += '</html></body></h1>'
+    page += '</html>'
     return page
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
